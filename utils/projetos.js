0e054e0e47e43af10b9671ca39c3adefee29f292
Marco Ferreira <marcoavfcc@gmail.com>
2021-02-23 10:19:20 -0300

    update projects

--- utils/projetos.js
+++ utils/projetos.js
@@ -2,17 +2,24 @@ import Site from '../src/images/WhatsApp.jpeg';
 import Blog from '../src/images/WhatsApp1.jpeg';
 import BlogPessoal from '../src/images/WhatsApp2.jpeg';
 import bloge from '../src/images/blog-e.png';
+import bloge1 from '../src/images/bloge.png';
 
 export default [
     {
         title: "Blog",
-        description: "Site desenvolvido com cms para administrar seus proprios conteudos",
+        description: "Blog desenvolvido com cms para administrar seus proprios conteudos",
+        url: "https://bloge1.netlify.app/",
+        image: bloge1
+    },
+    {
+        title: "Blog",
+        description: "Blog desenvolvido com cms para administrar seus proprios conteudos",
         url: "https://blogf.netlify.app/",
         image: bloge
     },
     {
         title: "Blog",
-        description: "Site desenvolvido em gatsby, markdown e styled componets",
+        description: "Blog desenvolvido em gatsby, markdown e styled componets",
         url: "https://blogmarco.netlify.app/",
         image: Blog
     },
